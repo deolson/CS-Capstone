a7cca9d512afc26c135c43e1bae01303dc21e7eb
Andy <avanheuit@pugetsound.edu>
2017-04-26 23:16:37 -0700

    fix output_midi.py

--- output_midi.py
+++ output_midi.py
@@ -1,7 +1,7 @@
-# state matrix returns all data in a dataset into a hashmap of state matrices
-# need a way to return a state matrix back into a midi file
-# using python 2.7 as a script, takes hashmap structure and writes it to data.txt,
-#   which is then ported into main.py and interpreted in 3.x
+# # state matrix returns all data in a dataset into a hashmap of state matrices
+# # need a way to return a state matrix back into a midi file
+# # using python 2.7 as a script, takes hashmap structure and writes it to data.txt,
+# #   which is then ported into main.py and interpreted in 3.x
 import numpy
 import midi
 import json
@@ -10,65 +10,50 @@ import os
 
 def StateMatrixtoMidi(StateMatrix):
     key = StateMatrix.keys()
+    StateMatrix = numpy.asarray(StateMatrix[key[0]])
+
+    total_notes = 128
+    tick_scale = 55
 
     pattern = midi.Pattern()
     track = midi.Track()
     pattern.append(track)
 
+
     # StateMatrix[key[0]], StateMatrix[key[0]][0], and StateMatrix[key[0]][0][0]
     # are lists
     # StateMatrix[key[0]][0][0][0] is an int
 
-    events = StateMatrix[key[0]]
-    for i in range(len(events)):
-        # print("---- %i ----" %(i))
-        for j in range(len(events[i])):
+    lastcmdtime = 0
 
-            # [1, 1] = Note started
-            if events[i][j] == [1, 1]:
-                on = midi.NoteOnEvent(tick=i, velocity=90, pitch=j)
-                track.append(on)
-                # print("midi.%s" %(str(getNote(j))))
+    prev_state = [[0,0] for x in range(total_notes)]
 
+    for time, state in enumerate(StateMatrix + prev_state):
+        off_notes = []
+        on_notes = []
+        for i in range(total_notes):
+            if prev_state[i][0] == 1:
+                if state[i][0] == 0:
+                    off_notes.append(i)
+                elif state[i][1] == 1:
+                    off_notes.append(i)
+                    on_notes.append(i)
+            elif state[i][0] == 1:
+                on_notes.append(i)
+        for note in off_notes:
+            track.append(midi.NoteOffEvent(tick=(time-lastcmdtime)*tick_scale, pitch = note + 24))
+            lastcmdtime = time
 
-            # [1, 0] = note held over from before
-            # if events[i][j] == [1, 0]:
-            #     print(getNote(j))
+        for note in on_notes:
+            track.append(midi.NoteOnEvent(tick=(time-lastcmdtime)*tick_scale, velocity = 40, pitch = note + 24))
+            lastcmdtime = time
 
-            # [0, 0] = note not playing
-            if events[i][j] == [0, 0] and events[i - 1][j] != [0, 0]:
-                off = midi.NoteOffEvent(tick=i, pitch=j)
-                track.append(off)
-                # print("%s off" %(getNote(j)))
+        prev_state = state
 
-    eot = midi.EndOfTrackEvent(tick=len(events))
+    eot = midi.EndOfTrackEvent(tick=len(StateMatrix))
     track.append(eot)
     print pattern
     midi.write_midifile("output.mid", pattern)
-
-
-def getNote(intNote):
-    oct = intNote/12
-    letter = switch(intNote%12)
-    return letter+str(oct)
-
-def switch(x):
-    return {
-        0 : 'C_',
-        1 : 'C#_',
-        2 : 'D_',
-        3 : 'D#_',
-        4 : 'E_',
-        5 : 'F_',
-        6 : 'F#_',
-        7 : 'G_',
-        8 : 'G#_',
-        9 : 'A_',
-        10 : 'A#_',
-        11 : 'B_',
-    }[x]
-
-
 
 
 if __name__ == '__main__':
